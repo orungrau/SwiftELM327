4147cab668ffa431a669e770e2bf2126f250105f
Anatoly Darkos <myaskov@me.com>
2017-08-06 11:36:05 +0300

    Add EasySocket

---
+++ Source/EasySocket.swift
@@ -0,0 +1,13 @@
+//
+//  EasySocket.swift
+//  SwiftELM327
+//
+//  Created by Anatoly Myaskov on 06.08.17.
+//  Copyright © 2017 Anatoly Myaskov. All rights reserved.
+//
+
+import UIKit
+
+class EasySocket: NSObject {
+
+}
